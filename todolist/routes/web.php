1aeec2e25122bb5b226b17b52f83dce766a41798
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-06-26 18:22:47 +0100

    covered get, post, put & delete and fallback()

--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -10,10 +10,14 @@ Route::get('/hello', function () {
     return 'hello';
 });
 
-Route::get('/hallo', function () {
-    return redirect()->route('hello');
+Route::get('/xxx', function () {
+    return redirect('/greet');
 });
 
 Route::get('/greet/{name}', function ($name) {
     return 'hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'this worked';
 });
