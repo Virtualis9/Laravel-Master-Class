6753cbd0005b717c27298247bd38dd84a8fa2f25
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-06-30 11:42:51 +0100

    From submission works. stores in database

--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -1,91 +1,51 @@
 <?php
-
+use App\models\Task;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-
-
-class Task
-{
-  public function __construct(
-    public int $id,
-    public string $title,
-    public string $description,
-    public ?string $long_description,
-    public bool $completed,
-    public string $created_at,
-    public string $updated_at
-  ) {
-  }
-}
-
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
-Route::get('/', function () use($tasks){
+Route::get('/', function () {
     return view('index', [
-        'tasks' =>$tasks
+        'tasks' => Task::latest()->where('completed', true)->get()
     ]);
 })->name('tasks.index');
 
+Route::view('/tasks/create', 'create') 
+    -> name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id) use($tasks) {
-    $task = collect($tasks)->firstWhere('id',$id);
-    if(!$task) {
-        return abort(Response::HTTP_NOT_FOUND);
-    }
-    return view('show', ['task' => $task]);
-    
 
-    
+Route::get('/tasks/{id}', function ($id) {
+return view('show', ['task' => Task::find($id)]);  
 })->name('tasks.show');
+
+
 
 Route::fallback(function () {
     return redirect()->route('tasks.index');
 });
 
+Route::post('/tasks', function(Request $request){
+   $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+   ]);
 
+   $task = new Task;
+   $task->title = $data['title'];
+   $task->description = $data['description'];
+   $task->long_description = $data['long_description'];
 
+   $task -> save();
+
+   return redirect()->route('tasks.show', ['id' => $task->id]);
+   
+})->name('tasks.store');
 
 
 // Route::get('/hello', function () {
